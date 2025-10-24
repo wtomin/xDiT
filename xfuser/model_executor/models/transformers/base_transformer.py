6c737573414d157750d9370d1a70e13467d1a8de
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-10-24 17:22:07 +0800

    more balanced pipeline

--- xfuser/model_executor/models/transformers/base_transformer.py
+++ xfuser/model_executor/models/transformers/base_transformer.py
@@ -108,14 +108,23 @@ class xFuserTransformerBaseWrapper(xFuserModelBaseWrapper, metaclass=ABCMeta):
             stage_block_end_idx = sum(attn_layer_num_for_pp[: pp_rank + 1])
 
         else:
-            num_blocks_per_stage = (
-                sum(num_blocks_list) + pp_world_size - 1
-            ) // pp_world_size
-            stage_block_start_idx = pp_rank * num_blocks_per_stage
-            stage_block_end_idx = min(
-                (pp_rank + 1) * num_blocks_per_stage,
-                sum(num_blocks_list),
-            )
+            # num_blocks_per_stage = (
+            #     sum(num_blocks_list) + pp_world_size - 1
+            # ) // pp_world_size
+            # stage_block_start_idx = pp_rank * num_blocks_per_stage
+            # stage_block_end_idx = min(
+            #     (pp_rank + 1) * num_blocks_per_stage,
+            #     sum(num_blocks_list),
+            # )
+            num_blocks_per_stage = sum(num_blocks_list) // pp_world_size
+            remainder = sum(num_blocks_list) % pp_world_size
+            block_dist = [
+                (i * num_blocks_per_stage + max(0, i - (pp_world_size - remainder)),
+                (i + 1) * num_blocks_per_stage + max(0, (i + 1) - (pp_world_size - remainder)),
+                )
+               for i in range(pp_world_size)
+            ]
+            stage_block_start_idx, stage_block_end_idx = block_dist[pp_rank]
 
         self.stage_info = StageInfo()
         for name, [blocks_start, blocks_end] in zip(
