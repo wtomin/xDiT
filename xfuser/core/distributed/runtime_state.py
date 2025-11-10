fix patches rotation

xfuser/core/distributed/runtime_state.py
@@ -18,6 +18,7 @@ except ModuleNotFoundError:
     pass
 
 import xfuser.envs as envs
+
 if envs._is_npu():
     from torch.npu import manual_seed as device_manual_seed
     from torch.npu import manual_seed_all as device_manual_seed_all
@@ -98,7 +99,7 @@ class UnetRuntimeState(RuntimeState):
     def __init__(self, pipeline: DiffusionPipeline, config: EngineConfig):
         super().__init__(config)
         self.sanity_check()
-    
+
     def sanity_check(self):
         if self.parallel_config.world_size > 1:
             if not(self.parallel_config.cfg_degree == 2 and self.parallel_config.world_size == 2):
@@ -232,15 +233,19 @@ class DiTRuntimeState(RuntimeState):
         self.backbone_inner_dim = backbone_inner_dim
         self.backbone_in_channel = backbone_in_channel
 
-    def set_patched_mode(self, patch_mode: bool):
+    def set_patched_mode(self, patch_mode: bool, initial_patch_idx: int = 0):
         self.patch_mode = patch_mode
-        self.pipeline_patch_idx = 0
+        self.pipeline_patch_idx = initial_patch_idx
 
-    def next_patch(self):
+    def next_patch(self, patch_idx: int | None = None):
         if self.patch_mode:
-            self.pipeline_patch_idx += 1
-            if self.pipeline_patch_idx == self.num_pipeline_patch:
-                self.pipeline_patch_idx = 0
+            if patch_idx is not None:
+                assert patch_idx < self.num_pipeline_patch, "Patch index out of range"
+                self.pipeline_patch_idx = patch_idx
+            else:
+                self.pipeline_patch_idx += 1
+                if self.pipeline_patch_idx == self.num_pipeline_patch:
+                    self.pipeline_patch_idx = 0
         else:
             self.pipeline_patch_idx = 0
 
@@ -680,4 +685,3 @@ def initialize_runtime_state(pipeline: DiffusionPipeline, engine_config: EngineC
         _RUNTIME = DiTRuntimeState(pipeline=pipeline, config=engine_config)
     elif hasattr(pipeline, "unet"):
         _RUNTIME = UnetRuntimeState(pipeline=pipeline, config=engine_config)
-

xfuser/model_executor/pipelines/pipeline_pixart_sigma.py
@@ -432,6 +432,8 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
         latents: torch.Tensor,
         t: Union[float, torch.Tensor],
         extra_step_kwargs: Dict,
+        first_patch: bool = False,
+        last_patch: bool = False,
     ):
         # compute previous image: x_t -> x_t-1
         return self.scheduler.step(
@@ -440,6 +442,8 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
             latents,
             **extra_step_kwargs,
             return_dict=False,
+            first_patch=first_patch,
+            last_patch=last_patch,
         )[0]
 
     # synchronized compute the whole feature map in each pp stage
@@ -474,6 +478,8 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
             else:
                 latents = get_pp_group().pipeline_recv()  # blocking recv
 
+            if is_pipeline_last_stage():
+                self._prev_inputs = latents.clone()
             latents = self._backbone_forward(
                 latents=latents,
                 prompt_embeds=prompt_embeds,
@@ -485,7 +491,15 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
 
             if is_pipeline_last_stage():
                 latents = self._scheduler_step(
-                    latents, last_timestep_latents, t, extra_step_kwargs
+                    latents,
+                    last_timestep_latents,
+                    t,
+                    extra_step_kwargs,
+                    first_patch=get_runtime_state().pipeline_patch_idx == 0,
+                    last_patch=(
+                        get_runtime_state().pipeline_patch_idx
+                        == get_runtime_state().num_pipeline_patch - 1
+                    ),
                 )
             if i == len(timesteps) - 1 or (
                 (i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0
@@ -560,48 +574,62 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
         patch_indices = np.roll(
             range(num_pipeline_patch), get_pipeline_parallel_rank()
         ).tolist()
+        get_runtime_state().set_patched_mode(
+            patch_mode=True, initial_patch_idx=patch_indices[0]
+        )
 
         for i, t in enumerate(timesteps):
             # logger.info(f"Step {i} Pipeline rank {get_pipeline_parallel_rank()}: {patch_indices}")
             # add communication queue
-            # `is_pipeline_first_stage() and i == 0` already has the input latents from the warmup stage
-            if not (is_pipeline_first_stage() and i == 0):
-                if is_pipeline_first_stage():
-                    # last stage sends and first stage receives patches in a different order
-                    last_patch_indices = np.roll(
-                        np.roll(
-                            range(num_pipeline_patch),
-                            get_pipeline_parallel_world_size() - 1,
-                        ),
-                        i - 1,
-                    )
-                    # logger.info(f"Step {i} Pipeline rank {get_pipeline_parallel_rank()}: {last_patch_indices} to receive")
-                    get_pp_group().add_pipeline_recv_tasks(last_patch_indices.tolist())
-                else:  # later stages use cached first patch from previous timestep
-                    get_pp_group().add_pipeline_recv_tasks(patch_indices[1:])
+            if is_pipeline_first_stage() and i != len(timesteps) - 1:
+                # last stage sends and first stage receives patches in a different order
+                last_patch_indices = np.roll(
+                    np.roll(
+                        range(num_pipeline_patch),
+                        get_pipeline_parallel_world_size() - 1,
+                    ),
+                    i,
+                )
+                get_pp_group().add_pipeline_recv_tasks(last_patch_indices.tolist())
+            # later stages use cached first patch from previous timestep
+            elif not is_pipeline_first_stage():
+                get_pp_group().add_pipeline_recv_tasks(patch_indices[1:])
+            # logger.info(f"Step {i} Pipeline rank {get_pipeline_parallel_rank()}: {get_pp_group().recv_tasks_queue}")
 
             for ip, patch_idx in enumerate(patch_indices):
+                get_runtime_state().next_patch(patch_idx)
+
                 if is_pipeline_last_stage():
                     last_patch_latents[patch_idx] = patch_latents[patch_idx]
 
                 # always re-use the initial patch from previous timestep / warmup step (except the first stage)
-                if ip != 0 or is_pipeline_first_stage():
-                    if not (is_pipeline_first_stage() and i == 0):
-                        with nvtx.range(f"async_recv_{i + num_warmup_steps}"):
-                            # get first n - 1 patches (the last one is still calculated)
-                            if not len(get_pp_group().receiving_tasks["latent"]):
-                                for _ in range(num_pipeline_patch - 1):
-                                    get_pp_group().recv_next()
-                            # blocking recv
-                            patch_latents[patch_idx] = (
-                                get_pp_group().get_pipeline_recv_data(idx=patch_idx)
-                            )
+                if (not is_pipeline_first_stage() and ip != 0) or (
+                    is_pipeline_first_stage() and i != 0
+                ):
+                    with nvtx.range(f"async_recv_{i + num_warmup_steps}"):
+                        # blocking recv
+                        patch_latents[patch_idx] = (
+                            get_pp_group().get_pipeline_recv_data(idx=patch_idx)
+                        )
 
                 with nvtx.range(f"async_computation_{i + num_warmup_steps}"):
-                    if not is_pipeline_first_stage() and ip == 0:
-                        # TODO: cache correction
+                    if not is_pipeline_last_stage() and ip == num_pipeline_patch - 1:
+                        # TODO: correct cached patches
                         pass
+                    elif is_pipeline_last_stage() and ip == 0:
+                        patch_latents[patch_idx] = self._backbone_forward(
+                            latents=self._prev_inputs[patch_idx],
+                            prompt_embeds=prompt_embeds,
+                            prompt_attention_mask=prompt_attention_mask,
+                            added_cond_kwargs=added_cond_kwargs,
+                            t=t,
+                            guidance_scale=guidance_scale,
+                        )
                     else:
+                        if is_pipeline_last_stage():
+                            self._prev_inputs[patch_idx] = patch_latents[
+                                patch_idx
+                            ].clone()
                         patch_latents[patch_idx] = self._backbone_forward(
                             latents=patch_latents[patch_idx],
                             prompt_embeds=prompt_embeds,
@@ -620,6 +648,8 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
                             last_patch_latents[patch_idx],
                             t,
                             extra_step_kwargs,
+                            first_patch=ip == 0,
+                            last_patch=ip == num_pipeline_patch - 1,
                         )
 
                     if i != len(timesteps) - 1:
@@ -636,13 +666,7 @@ class xFuserPixArtSigmaPipeline(xFuserPipelineBaseWrapper):
 
                 if len(get_pp_group().recv_tasks_queue):
                     with nvtx.range(f"async_recv_{i + num_warmup_steps}"):
-                        if is_pipeline_first_stage():
-                            if ip == 0:  # get the last patch
-                                get_pp_group().recv_next()
-                        else:
-                            get_pp_group().recv_next()  # add receive task, non-blocking
-
-                get_runtime_state().next_patch()
+                        get_pp_group().recv_next()  # add receive task, non-blocking
 
             if i == len(timesteps) - 1 or (
                 (i + num_pipeline_warmup_steps + 1) > num_warmup_steps

xfuser/model_executor/schedulers/scheduling_dpmsolver_multistep.py
@@ -26,6 +26,8 @@ class xFuserDPMSolverMultistepSchedulerWrapper(xFuserSchedulerBaseWrapper):
         generator=None,
         variance_noise: Optional[torch.Tensor] = None,
         return_dict: bool = True,
+        first_patch: bool = False,
+        last_patch: bool = False,
     ) -> Union[SchedulerOutput, Tuple]:
         """
         Predict the sample from the previous timestep by reversing the SDE. This function propagates the sample with
@@ -77,7 +79,7 @@ class xFuserDPMSolverMultistepSchedulerWrapper(xFuserSchedulerBaseWrapper):
         #! ---------------------------------------- MODIFIED BELOW ----------------------------------------
         if (
             get_runtime_state().patch_mode
-            and get_runtime_state().pipeline_patch_idx == 0
+            and first_patch
             and self.model_outputs[-1] is None
         ):
             self.model_outputs[-1] = torch.zeros(
@@ -90,13 +92,13 @@ class xFuserDPMSolverMultistepSchedulerWrapper(xFuserSchedulerBaseWrapper):
                 device=model_output.device,
                 dtype=model_output.dtype,
             )
-        if get_runtime_state().pipeline_patch_idx == 0:
+        if first_patch:
             for i in range(self.config.solver_order - 1):
                 self.model_outputs[i] = self.model_outputs[i + 1]
 
         if (
             get_runtime_state().patch_mode
-            and get_runtime_state().pipeline_patch_idx == 0
+            and first_patch
         ):
             assert len(self.model_outputs) >= 2
             self.model_outputs[-1] = torch.zeros_like(self.model_outputs[-2])
@@ -188,11 +190,7 @@ class xFuserDPMSolverMultistepSchedulerWrapper(xFuserSchedulerBaseWrapper):
 
         # upon completion increase step index by one
         # * increase step index only when the last pipeline patch is done (or not in patch mode)
-        if (
-            not get_runtime_state().patch_mode
-            or get_runtime_state().pipeline_patch_idx
-            == get_runtime_state().num_pipeline_patch - 1
-        ):
+        if not get_runtime_state().patch_mode or last_patch:
             self._step_index += 1
 
         if not return_dict: